86ba541e36bfa24915e71f271446a7b09843900a
Vitaliy Nickishin <vnickishin@202-ecommerce.com>
2025-03-27 23:44:34 +0200

    back,block-post-heading-link,added class

--- acf-components/block-post-case-results.php
+++ acf-components/block-post-case-results.php
@@ -1,22 +1,24 @@
 <?php
 $results = get_field('post_case_results_items');
-$result_groups = array_chunk($results, 2);
+if (!empty($results)) :
+    $result_groups = array_chunk($results, 2);
 ?>
-<section class="section-result my-lg-3 py-5 py-lg-4">
-    <h2 class="mb-3 section-title"><?php the_field('post_case_results_title'); ?></h2>
-    <div class="resault-boxes row">
-        <?php if (!empty($results)): ?>
-            <?php foreach($result_groups as $result_items) : ?>
-                <?php foreach($result_items as $result_item) : ?>
-                    <div class="col col-12 <?php echo count($result_items) == 1 ? '' : 'col-lg-6'; ?>">
-                        <div class="serv-box ">
-                            <span class="number mb-2"><?php echo $result_item['title']; ?></span>
-                            <p><?php echo $result_item['text']; ?></p>
+    <section class="section-result my-lg-3 py-5 py-lg-4">
+        <h2 class="mb-3 section-title"><?php the_field('post_case_results_title'); ?></h2>
+        <div class="resault-boxes row">
+            <?php if (!empty($results)): ?>
+                <?php foreach ($result_groups as $result_items) : ?>
+                    <?php foreach ($result_items as $result_item) : ?>
+                        <div class="col col-12 <?php echo count($result_items) == 1 ? '' : 'col-lg-6'; ?>">
+                            <div class="serv-box ">
+                                <span class="number mb-2"><?php echo $result_item['title']; ?></span>
+                                <p><?php echo $result_item['text']; ?></p>
+                            </div>
                         </div>
-                    </div>
-                    
+
+                    <?php endforeach; ?>
                 <?php endforeach; ?>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-</section>
+            <?php endif; ?>
+        </div>
+    </section>
+<?php endif; ?>
--- acf-components/block-post-heading-link.php
+++ acf-components/block-post-heading-link.php
@@ -1,4 +1,11 @@
-<h2 class="wp-block-heading d-flex align-items-center">
+<?php
+$className = 'wp-block-heading d-flex align-items-center';
+if( !empty($block['className']) ) {
+	$className .= ' ' . $block['className'];
+}
+?>
+
+<h2 class="<?php echo $className; ?>">
     <?php the_field('post_heading_link_title'); ?>
     <a class="copy-link-to-heading" target="_blank" href="<?php the_field('post_heading_link_url'); ?>"></a>
 </h2>
