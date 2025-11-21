cdaaa5dfb53e173ec468e23c4725da6596e6d193
Vitaliy Nickishin <vnickishin@202-ecommerce.com>
2025-11-22 00:12:13 +0200

    implementation sub menu banner

--- template-parts/menu/sub-menu-banner.php
+++ template-parts/menu/sub-menu-banner.php
@@ -1,11 +1,14 @@
+<?php
+$sub_menu_banner = get_field('icoda_gb_header_sub_menu_banner', 'option');
+?>
 <div class="sub-menu-banner">
     <div class="text-center">
         <p class="title fw-semibold">
-            <?php _e('🚀 Our Projects speak louder than words', 'icoda'); ?>
+            <?php echo $sub_menu_banner['banner_title']; ?>
         </p>
         <div class="description">
-        <?php _e('Let ICODA power your journey to crypto dominance.', 'icoda'); ?>
+            <?php echo $sub_menu_banner['banner_description']; ?>
         </div>
-        <a href="/cases" class="btn btn-blue text-capitalize"><?php _e('Explore cases', 'icoda'); ?></a>
+        <a href="<?php echo $sub_menu_banner['page_url']; ?>" class="btn btn-blue text-capitalize"><?php echo $sub_menu_banner['button_text']; ?></a>
     </div>
 </div>
