1534b940acf1c4f05574a03e58d08371258b1698
Vitaliy Nickishin <vnickishin@202-ecommerce.com>
2026-02-19 16:27:07 +0200

    added style btn data-get-book afger use stripe pay method

--- template-parts/_partials/book-info.php
+++ template-parts/_partials/book-info.php
@@ -5,11 +5,11 @@ $icoda_gb_book_info = get_field('icoda_gb_book_info', 'option');
 							
     <div class="book-image position-relative">
         <picture>
-            <!-- <source srcset="<?php echo $icoda_gb_book_info['book_info']['book_image_mobile']['url']; ?>" media="(max-width: 600px)" /> -->
+            <!-- <source srcset="<?php //echo $icoda_gb_book_info['book_info']['book_image_mobile']['url']; ?>" media="(max-width: 600px)" /> -->
             <img 
-                data-src="<?php echo $icoda_gb_book_info['book_image_desktop']['url']; ?>" 
-                alt="<?php echo $icoda_gb_book_info['book_image']['book_alt']; ?>" 
-                src="<?php echo $icoda_gb_book_info['book_image_desktop']['url']; ?>" 
+                data-src="<?php if (!empty($icoda_gb_book_info['book_image_desktop']['url'])) { echo $icoda_gb_book_info['book_image_desktop']['url']; } ?>" 
+                alt="<?php if (!empty($icoda_gb_book_info['book_image']['book_alt'])){ echo $icoda_gb_book_info['book_image']['book_alt']; } ?>" 
+                src="<?php if (!empty($icoda_gb_book_info['book_image_desktop']['url'])) { echo $icoda_gb_book_info['book_image_desktop']['url']; } ?>" 
                 class="lazyloaded"
                 width="250" height="335"
                 >
--- template-parts/_partials/book-price.php
+++ template-parts/_partials/book-price.php
@@ -6,5 +6,6 @@ $icoda_gb_book_info = get_field('icoda_gb_book_info', 'option');
     <span class="book-price fw-semibold">
         $<?php echo $icoda_gb_book_info['book_price']; ?>
     </span>
-    <button data-get-book type="button" class="btn btn-blue"><?php echo $icoda_gb_book_info['button_text']; ?></button>
+    <!--<button data-get-book type="button" class="btn btn-blue"><?php //echo $icoda_gb_book_info['button_text']; ?></button>-->
+	<?php echo do_shortcode( '[wpecpp id="209914"]' ); ?>
 </div>
